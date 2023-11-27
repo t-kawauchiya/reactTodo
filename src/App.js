dfe75339336ec5520beb8ec510b40749fbcf8366
t-kawauchiya <t.kawauchiya@gmail.com>
2023-11-27 23:02:16 +0900

    add addTodo,removeTodo,toggleIsDone function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = () => {
+    if (!task) return;
+    setTodoList([...todoList, { task: task, isdone: false }]);
+    setTask("");
+  }
+  const removeTodo = (index) => {
+    const newList = [...todoList];
+    newList.splice(index, 1);
+    setTodoList(newList);
+  }
+  const toggleIsdone = (index) => {
+    const newList = [...todoList];
+    newList[index].isdone = !newList[index].isdone;
+    setTodoList(newList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((todo, index) => {
+          return <li key={index}>{todo.task}
+            <button onClick={() => toggleIsdone(index)} >{todo.isdone ? "Done" : "Not Done"}</button>
+            <button onClick={() => removeTodo(index)} >Remove</button>
+          </li>;
+        })}
+      </ul>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addTodo();
+      }}>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+        />
+        <input type="submit" value="Add Todo" onSubmit={addTodo} />
+      </form>
     </div>
   );
 }
